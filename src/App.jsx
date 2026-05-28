5a208ee8bc2c2df6c520002407ffcda8b5572954
veerachart <mak25388@gmail.com>
2026-05-28 11:46:46 +0700

    light theme

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <div
       style={{
-        background: "#f5f5f5",
+        background: "#ee2525",
         minHeight: "100vh",
         color: "white",
         padding: 20,
@@ -143,7 +143,7 @@ function App() {
         );
       }}
       style={{
-        background: "#white",
+        background: "#111",
         border: "none",
         color: "white",
         padding: "10px 15px",
